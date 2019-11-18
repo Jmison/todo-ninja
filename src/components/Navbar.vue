4dadff6f8722292391fb984c3aadfaaa608f4094
Jamo <jamo@Jamos-MBP.fios-router.home>
2019-11-18 09:40:44 -0500

    avatars added to the team page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,7 @@
       </v-btn>
     </v-toolbar>
 
-    <v-navigation-drawer app v-model="drawer" class="primary">
+    <v-navigation-drawer app v-model="drawer" class="indigo">
       <v-list>
         <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-tile-action>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,15 +5,22 @@
     <v-container class="my-5">
 
       <v-layout row class="mb-3">
-        <v-btn small flat color="grey" @click="sortBy('title')">
+        <v-tooltip top>
+           <v-btn small flat color="grey" @click="sortBy('title')" slot="activator">
           <v-icon left small>folder</v-icon>
           <span class="caption text-lowercase">By project name</span>
-        </v-btn>
+         </v-btn>
+          <span>Sort project by project name</span>
+        </v-tooltip>
 
-         <v-btn small flat color="grey" @click="sortBy('person')">
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
           <v-icon left small>person</v-icon>
           <span class="caption text-lowercase">By person</span>
         </v-btn>
+        <span>Sort project by person name</span>
+        </v-tooltip>
+
       </v-layout>
       
       <v-card flat v-for="project in projects" :key="project.title">
