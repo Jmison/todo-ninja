785927f8a61e2ad87d18a08377b93de2ce96e02a
Jamo <jamo@Jamos-MBP.fios-router.home>
2019-11-19 15:32:04 -0500

    Updated code, menu button fixed along with add project button added.

---
+++ src/components/Popup.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-dialog max-width="600px">
+      <v-btn flat slot="activator" class="succcess">Add new project</v-btn>
+      <v-card>
+          <v-card-title>
+              <h2>Add a project</h2>
+          </v-card-title>
+          <v-card-text>
+              <v-form class="px-3">
+                  <v-text-field label="title" v-model="title" prepend-icon="folder"></v-text-field>
+                  <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+                  <v-btn flat class="success mx-0 mt-3" @click="submit">Add project</v-btn>
+              </v-form>
+          </v-card-text>
+      </v-card>
+
+  </v-dialog>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            // empty title because it takes what the user inserts
+            title: '',
+            content: ''
+        }
+    },
+    methods: {
+        submit(){
+            console.log(this.title, this.content)
+        }
+    }
+}
+</script>
+
+<style>
+</style>
